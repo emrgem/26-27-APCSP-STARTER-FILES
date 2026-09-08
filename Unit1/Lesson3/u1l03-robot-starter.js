e7178819f882881a9107aa45c2d080db64972a2e
Emre Gemici <35531700+emrgem@users.noreply.github.com>
2026-09-07 22:25:55 -0400

    Create robot starter template in u1l03-robot-starter.js
    Initial setup for robot drawing with canvas and shapes.

---
+++ Unit1/Lesson3/u1l03-robot-starter.js
@@ -0,0 +1,25 @@
+// AP CSP - Unit 1, Lesson 3
+// Classwork: build a robot together
+//
+// We add to this as we go. Keep it open.
+
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(24, 38, 60);
+  noStroke();
+
+  // TODO: the head        rect
+
+  // TODO: two eyes        circle
+
+  // TODO: a mouth         ellipse
+
+  // TODO: antenna stalk   rect
+
+  // TODO: antenna bulb    circle
+
+}
